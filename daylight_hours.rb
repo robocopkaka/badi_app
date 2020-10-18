2d7aef9f02878ff27d8d750ae3a6964fd832f87e
robocopkaka <robocopkaka@gmail.com>
2020-10-18 04:49:51 +0100

    fix some rubocop issues

--- daylight_hours.rb
+++ daylight_hours.rb
@@ -1,84 +1,47 @@
 # frozen_string_literal: true
 
+require_relative 'time_hashes'
 # class for methods needed for both `init` and `getDaylightHours`
 class DaylightHours
-  @left_time_hash = {
-    '0' => '08:14:00',
-    '1' => '08:14:00',
-    '2' => '08:14:00',
-    '3' => '08:44:00',
-    '4' => '08:44:00',
-    '5' => '08:44:00',
-    '6' => '09:14:00',
-    '7' => '09:14:00',
-    '8' => '09:14:00',
-    '9' => '09:44:00',
-    '10' => '09:44:00',
-    '11' => '09:44:00',
-    '12' => '10:14:00',
-    '13' => '10:14:00',
-    '14' => '10:14:00',
-    '15' => '10:44:00',
-    '16' => '10:44:00',
-    '17' => '10:44:00'
-  }
-
-  @right_time_hash = {
-    '0' => '17:25:00',
-    '1' => '17:25:00',
-    '2' => '17:25:00',
-    '3' => '16:44:00',
-    '4' => '16:44:00',
-    '5' => '16:44:00',
-    '6' => '16:14:00',
-    '7' => '16:14:00',
-    '8' => '16:14:00',
-    '9' => '15:44:00',
-    '10' => '15:44:00',
-    '11' => '15:44:00',
-    '12' => '15:14:00',
-    '13' => '15:14:00',
-    '14' => '15:14:00',
-    '15' => '14:44:00',
-    '16' => '14:44:00',
-    '17' => '14:44:00'
-  }
-
   def self.prep_array(params)
-    error_message = validate_apartment_height(params)
-    return { error:  error_message } if error_message
-
     main_array = []
     neighbourhoods_hash =
       Hash.new { |key, value| key[value] = Hash.new(&key.default_proc) }
     params.each do |neighbourhood|
       nb_name = neighbourhood['neighbourhood']
       neighbourhoods_hash[nb_name]['start_index'] = main_array.length
-      neighbourhood['buildings'].each do |building|
-        main_array.push Array(0..building['apartments_count'] - 1)
-        neighbourhoods_hash[nb_name]['buildings'][building['name']] = main_array.length - 1
-      end
+      create_array(main_array, neighbourhoods_hash, neighbourhood)
       neighbourhoods_hash[nb_name]['end_index'] = main_array.length - 1
     end
     { hash: neighbourhoods_hash.to_json, array: main_array }
   end
 
-  def self.higher_to_left?(cache_handler, building_index, apartment_number, first)
-    array = cache_handler.get(:nb_array)
-    return true if building_index == first
+  def self.create_array(main_array, neighbourhoods_hash, neighbourhood)
+    name = neighbourhood['neighbourhood']
+    neighbourhood['buildings'].each do |building|
+      main_array.push Array(0..building['apartments_count'] - 1)
 
-    (building_index - 1).downto(first) do |index|
+      neighbourhoods_hash[name]['buildings'][building['name']] =
+        main_array.length - 1
+    end
+  end
+
+  def self.higher_to_left?(cache_handler, building, apartment_number, first)
+    array = cache_handler.get(:nb_array)
+    return true if building == first
+
+    (building - 1).downto(first) do |index|
       return false unless array[index][apartment_number].nil?
     end
 
     true
   end
 
-  def self.higher_to_right?(cache_handler, building_index, apartment_number, last)
+  def self.higher_to_right?(cache_handler, building, apartment_number, last)
     array = cache_handler.get(:nb_array)
-    return true if building_index == last
+    return true if building == last
 
-    (building_index + 1).upto(last) do |index|
+    (building + 1).upto(last) do |index|
       return false unless array[index][apartment_number].nil?
     end
 
@@ -111,22 +74,22 @@ class DaylightHours
     start_hour_east = east_hours.first.to_s
     end_hour_west = west_hours.first.to_s
 
-    return "#{@left_time_hash[start_hour_east]} - 14:14:00" if west_hours.empty?
-    return "11:14:00 - #{@right_time_hash[end_hour_west]}" if east_hours.empty?
+    return "#{LEFT_TIME_HASH[start_hour_east]} - 14:14:00" if west_hours.empty?
+    return "11:14:00 - #{RIGHT_TIME_HASH[end_hour_west]}" if east_hours.empty?
 
-    "#{@left_time_hash[start_hour_east]} - #{@right_time_hash[end_hour_west]}"
+    "#{LEFT_TIME_HASH[start_hour_east]} - #{RIGHT_TIME_HASH[end_hour_west]}"
   end
 
   def self.validate_apartment_height(params)
     params.each do |nb|
-      heights = nb['buildings']
-                .map { |val| val['apartments_count'] }
-                .sort
+      heights = nb['buildings'].map { |val| val['apartments_count'] }.sort
       if heights.last > 18
         return "You have an apartment with height higher \
                 than 18 in #{nb['neighbourhood']}. Maximum \
                 number of apartments allowed for a building is 18"
       end
     end
+
+    nil
   end
 end
--- index.rb
+++ index.rb
@@ -1,60 +1,77 @@
+# frozen_string_literal: true
+
 require 'sinatra/base'
 require 'pry'
 require 'dalli'
 require_relative 'daylight_hours'
 
+# entry point for app
 class DaylightApp < Sinatra::Application
   set :port, 3000
   enable :logging, :sessions
   set :dc, Dalli::Client.new('localhost:11211')
   attr_accessor :app_instance
-  
+
   helpers do
     def json_params
       JSON.parse(request.body.read)
-    rescue
-      halt 400, { message: "Invalid JSON" }.to_json
+    rescue JSONError
+      halt 400, { message: 'Invalid JSON' }.to_json
     end
   end
 
-  def set_cache key, value
+  def set_cache(key, value)
     settings.dc.set(key, value)
     value
   end
-  
+
   get '/init' do
-    result = DaylightHours.prep_array(json_params["neighbourhoods"])
+    parameters = json_params['neighbourhoods']
+    error = DaylightHours.validate_apartment_height(parameters)
+    halt 400, { message: error }.to_json if error
+    result = DaylightHours.prep_array(parameters)
     hash = result[:hash]
     array = result[:hash]
-    error = result[:error]
-    halt 400, { message: error }.to_json if error
+
     set_cache(:nb_array, array)
     set_cache(:nb_hash, hash)
   end
-  
+
   get '/daylight_hours' do
     cache_handler = settings.dc
     query = json_params
-    neighbourhood = query["neighbourhood"]
-    building = query["building"]
-    apartment_number = query["apartment_number"].to_i
+    neighbourhood = query['neighbourhood']
+    building = query['building']
+    apartment_number = query['apartment_number'].to_i
     hash = JSON.parse(cache_handler.get(:nb_hash))
-    building_index = hash[neighbourhood]["buildings"][building]
-    first = hash[neighbourhood]["start_index"]
-    last = hash[neighbourhood]["end_index"]
-    # binding.pry
+    building_index = hash[neighbourhood]['buildings'][building]
+    first = hash[neighbourhood]['start_index']
+    last = hash[neighbourhood]['end_index']
+
     higher_to_left = DaylightHours
-      .higher_to_left?(
-        cache_handler, building_index, apartment_number - 1, first
-      )
+                     .higher_to_left?(
+                       cache_handler,
+                       building_index,
+                       apartment_number - 1,
+                       first
+                     )
     higher_to_right = DaylightHours
-      .higher_to_right?(
-        cache_handler, building_index, apartment_number - 1, last
-      )
+                      .higher_to_right?(
+                        cache_handler,
+                        building_index,
+                        apartment_number - 1,
+                        last
+                      )
     east_hours = west_hours = []
-    east_hours = DaylightHours.add_hours_left(apartment_number - 1, building_index, first) if higher_to_left
-    west_hours = DaylightHours.add_hours_right(apartment_number - 1, building_index, last) if higher_to_right
-    
+    if higher_to_left
+      east_hours = DaylightHours
+                   .add_hours_left(apartment_number - 1, building_index, first)
+    end
+    if higher_to_right
+      west_hours = DaylightHours
+                   .add_hours_right(apartment_number - 1, building_index, last)
+    end
+
     DaylightHours.compute_total_hours(east_hours, west_hours)
   end
 end
